7b113c02b0dff64f28efdd03fd00766495a7defd
Nirdesh Pokharel <mail@nirdeshpokhrel.com.np>
2023-03-29 07:31:54 +0545

    added fisr, middle and last name of user

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,4 +1,5 @@
 
+import {Role} from '@prisma/client'
 import {ApiProperty} from '@nestjs/swagger'
 import {Transaction} from '../../transaction/entities/transaction.entity'
 import {UserProgress} from '../../userProgress/entities/userProgress.entity'
@@ -8,8 +9,14 @@ import {Otp} from '../../otp/entities/otp.entity'
 export class User {
   id: string ;
 username: string ;
+first_name: string ;
+middle_name: string  | null;
+last_name: string ;
 email: string ;
-type: string ;
+@ApiProperty({
+  enum: Role,
+})
+type: Role  | null;
 @ApiProperty({
   type: `string`,
   format: `date-time`,

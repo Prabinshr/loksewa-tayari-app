3e49c79020329b5b5058d9812c7fdde1cdf235a2
Nirdesh Pokharel <mail@nirdeshpokhrel.com.np>
2023-04-01 17:00:46 +0545

    removed unnecessary  enum

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,6 +1,1 @@
 export const jwtSecret = process.env.JWT_SECRET;
-export enum OTPType {
-    EMAIL_VERIFICATION = 'EMAIL_VERIFICATION',
-    PASSWORD_RESET = 'PASSWORD_RESET',
-    OTHER = 'OTHER'
-}

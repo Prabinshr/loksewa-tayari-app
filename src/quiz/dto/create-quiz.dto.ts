6260acd19d33c2fcc517518a93470c6f391cb81d
Prashant Gurung <prasantgrg777@gmail.com>
2023-05-08 16:15:05 +0545

    category_id added in Dto

--- src/quiz/dto/create-quiz.dto.ts
+++ src/quiz/dto/create-quiz.dto.ts
@@ -1,30 +1,32 @@
-
-import {Type} from 'class-transformer'
-import {IsNotEmpty,IsNumber,IsOptional,IsString} from 'class-validator'
-import {ApiProperty,getSchemaPath} from '@nestjs/swagger'
-
-
-
+import { Type } from 'class-transformer';
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import { ApiProperty, getSchemaPath } from '@nestjs/swagger';
 
 export class CreateQuizDto {
   @ApiProperty()
-@IsNotEmpty()
-@IsString()
-name: string;
-@ApiProperty({
-  type: `number`,
-  format: `float`,
-  default: 0,
-})
-@IsOptional()
-@IsNumber()
-cost?: number;
-@ApiProperty({
-  type: `number`,
-  format: `float`,
-  default: 0,
-})
-@IsOptional()
-@IsNumber()
-negative_mark_value?: number;
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  // @IsNotEmpty()
+  @IsString()
+  category_id: string;
+  
+  @ApiProperty({
+    type: `number`,
+    format: `float`,
+    default: 0,
+  })
+  @IsOptional()
+  @IsNumber()
+  cost?: number;
+  @ApiProperty({
+    type: `number`,
+    format: `float`,
+    default: 0,
+  })
+  @IsOptional()
+  @IsNumber()
+  negative_mark_value?: number;
 }

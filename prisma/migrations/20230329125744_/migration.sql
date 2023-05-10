5d1083587f103c18e3306a5130d5c97a8236af04
Bibek Acharya <bibekacharya955@gmail.com>
2023-05-10 21:45:42 +0545

    all migartions removed

--- prisma/migrations/20230329125744_/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "User_Progress" DROP CONSTRAINT "User_Progress_user_id_fkey";
-
--- AddForeignKey
-ALTER TABLE "User_Progress" ADD CONSTRAINT "User_Progress_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
--- prisma/migrations/20230503081700_coupon_used_count/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `maxUses` to the `Coupon` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Coupon" ADD COLUMN     "maxUses" INTEGER NOT NULL,
-ADD COLUMN     "usedCount" INTEGER NOT NULL DEFAULT 0;

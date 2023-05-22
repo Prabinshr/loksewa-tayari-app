5425948306d155b58aabb4881fe20d1f95bd3099
Krish Magar <krishmagar010@gmail.com>
2023-05-22 15:21:32 +0545

    Added Swagger In Exams Controller

--- src/exams/exams.controller.ts
+++ src/exams/exams.controller.ts
@@ -6,20 +6,25 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { ExamsService } from './exams.service';
 import { CreateExamDto } from './dto/create-exam.dto';
 import { UpdateExamDto } from './dto/update-exam.dto';
+import { ApiBearerAuth, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags("Dynamic SubService's Exam Set")
+@ApiBearerAuth('jwt')
 @Controller()
 export class ExamsController {
   constructor(private readonly examsService: ExamsService) {}
 
+  @ApiOperation({ summary: "Gets Specific SubService's Exam Set" })
   @Get(':sewaService/:subService/examsets/:mock?')
   findAll(
     @Param('sewaService') sewaService: string,
     @Param('subService') subService: string,
-    @Param('mock') mock: number,
+    @Param('mock') mock?: number,
   ) {
     return this.examsService.findAllSets(sewaService, subService, mock);
   }
